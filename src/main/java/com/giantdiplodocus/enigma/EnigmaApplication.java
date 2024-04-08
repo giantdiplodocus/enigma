8e6e62bbfc172fdf8be7cdd4be0a19a7dcb97f6a
Jasim Aboobacker <jasim.contacts@gmail.com>
2024-04-09 00:53:16 +0200

    Initial commit

--- src/main/java/com/giantdiplodocus/enigma/EnigmaApplication.java
+++ src/main/java/com/giantdiplodocus/enigma/EnigmaApplication.java
@@ -1,10 +1,7 @@
 package com.giantdiplodocus.enigma;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EnigmaApplication{
--- src/test/java/com/giantdiplodocus/enigma/controller/EnigmaControllerTest.java
+++ src/test/java/com/giantdiplodocus/enigma/controller/EnigmaControllerTest.java
@@ -2,12 +2,10 @@ package com.giantdiplodocus.enigma.controller;
 
 import com.giantdiplodocus.enigma.model.EnigmaRequest;
 import com.giantdiplodocus.enigma.service.EnigmaService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
